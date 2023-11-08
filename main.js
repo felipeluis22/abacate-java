105552aab76934e26027717a8765d7d230b12a54
luis felipe <114739100+felipeluis22@users.noreply.github.com>
2023-11-08 09:27:47 -0300

    llpopo

--- main.js
+++ main.js
@@ -7,19 +7,12 @@ function tocaSom(IDaudio){
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 //invocando a função tocaSomPom a partir do item 0, 1º botão
-listaDeTeclas [0].onclick = tocaSom;
-
 //criando referência variável "contador" iniciando = 0
-let contador = 0; 
-
 //estrutura de repetição while = enquanto
-while(contador < listaDeTeclas.length){
-    const efeito = listaDeTeclas[contador].classList[1];
-    const IDaudio = "#som_"+efeito;
-    listaDeTeclas[contador].onclick = function(){
+for(let contador = 0; contador < listaDeTeclas.length;contador++){
+    const tecla = listaDeTeclas[contador];
+    const efeito = tecla.classList[1];
+    const IDaudio = `#som_${efeito}`;
+    tecla.onclick = function(){
         tocaSom(IDaudio);
     }
-    contador = contador + 1;
-
-    console.log(contador);
-}
