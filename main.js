bf21814e361cb66097d7ce2ac30652cf68b43188
luis felipe <114739100+felipeluis22@users.noreply.github.com>
2023-11-21 11:30:26 -0300

    jj

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 //criando/declarando a função tocaSom
 function tocaSom(IDaudio){
-    document.querySelector(IDaudio).play();
-}
+    const elemento = document.querySelector(IDaudio);
+    if (elemento && elemento.locaNine === ' audio'){
+    
+    elemento.play();
+}else{ 
+    console.log("elemento não encontrado")
 
+  }
+}
 //criando referência constante ListaDeTeclas buscando todos
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
@@ -17,7 +23,7 @@ for(let contador = 0; contador < listaDeTeclas.length;contador++){
         tocaSom(IDaudio);
     }
    tecla.onkeydown = function (evento){
-if(evento.code != 'Tab'){
+if(evento.code === 'Space' || evento.code === "enter"){
 tecla.classList.add('.ativa');
 }
    }
